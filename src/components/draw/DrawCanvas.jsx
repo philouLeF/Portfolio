dd22b049bd991f428caea7af930078add780a13c
philouLeF <delcroixphilippe73@gmail.com>
2023-05-20 23:32:33 +0200

    draw section

--- src/components/draw/DrawCanvas.jsx
+++ src/components/draw/DrawCanvas.jsx
@@ -1,26 +1,60 @@
 import { useEffect, useRef } from "react";
+import { getCoordinates } from "../../lib/canvas";
 
-// Draw exercise
 export const DrawCanvas = ({ canvas }) => {
   const isDrawing = useRef(false);
   const lastCoordinate = useRef(null);
 
-  const startDrawing = (event) => {};
+  const startDrawing = (event) => {
+    isDrawing.current = true;
+    lastCoordinate.current = getCoordinates(event, canvas.current);
+  };
 
-  const stopDrawing = () => {};
+  const stopDrawing = () => {
+    isDrawing.current = false;
+    lastCoordinate.current = null;
+  };
 
-  const draw = (event) => {};
+  const draw = (event) => {
+    if (!isDrawing.current) return;
+    const context = canvas.current?.getContext("2d");
+    const coordinate = getCoordinates(event, canvas.current);
 
-  useEffect(() => {}, []);
+    if (!context || !coordinate) return;
+
+    if (lastCoordinate.current) {
+      context.lineCap = "round";
+      context.lineJoin = "round";
+      context.beginPath();
+      context.moveTo(lastCoordinate.current.x, lastCoordinate.current.y);
+      context.lineTo(coordinate.x, coordinate.y);
+      context.stroke();
+    }
+
+    lastCoordinate.current = coordinate;
+  };
+
+  useEffect(() => {
+    const handleMouseUp = () => {
+      stopDrawing();
+    };
+
+    window.addEventListener("mouseup", handleMouseUp);
+    return () => {
+      window.removeEventListener("mouseup", handleMouseUp);
+    };
+  }, []);
 
   return (
-    <canvas
-      onMouseDown={startDrawing}
-      onMouseMove={draw}
-      width={560}
-      height={315}
-      ref={canvas}
-      className="m-auto rounded-md bg-white shadow-md"
-    />
+    <div className="max-w-[90vw] overflow-hidden">
+      <canvas
+        onMouseDown={startDrawing}
+        onMouseMove={draw}
+        width={560}
+        height={315}
+        ref={canvas}
+        className="m-auto rounded-md bg-white shadow-md"
+      />
+    </div>
   );
 };
